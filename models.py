9ed373b2caf6b173b8e1536d79c3c771daa7b399
Brian Kibet <kibetsbrian@gmail.com>
2023-10-27 15:14:10 +0300

    create job model

--- models.py
+++ models.py
@@ -16,3 +16,20 @@ class Employee(db.Model):
         self.email = email
         self.skills = skills
         self.experience = experience
+        
+
+class Job(db.Model):
+   
+    __tablename__ = 'jobs'
+
+    id = db.Column(db.Integer, primary_key=True) 
+    title = db.Column(db.String)
+    description = db.Column(db.Text)
+    location = db.Column(db.String)
+    type = db.Column(db.String)
+
+    def __init__(self, title, description, location, type):
+        self.title = title
+        self.description = description
+        self.location = location
+        self.type = type
